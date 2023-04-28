d86ea3cc74b4089f7296f2685ef647e30d4c7c77
John Mathew <codredd@outlook.com>
2023-04-28 19:23:16 +0200

    feat: added a simple form with TDD

--- src/App.tsx
+++ src/App.tsx
@@ -1,2 +1,34 @@
-import React from 'react';
-export const App = () => <div>This is an awesome new react app deployed via CI/CD pipeline</div>;
+import React, { useState } from 'react';
+import { Input, Button } from '@mui/material';
+export const App = () => {
+  const [itemDescription, setItemDescription] = useState<string>('');
+  const [items, setItems] = useState<string[]>([]);
+
+  return (
+    <div>
+      <Input
+        value={itemDescription}
+        onChange={(event) => {
+          event.preventDefault();
+          setItemDescription(event.target.value);
+        }}
+      />
+      <Button
+        onClick={() => {
+          if (itemDescription !== '') {
+            setItems([...items, itemDescription]);
+            setItemDescription('');
+          }
+        }}
+      >
+        Create
+      </Button>
+
+      <ul>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
